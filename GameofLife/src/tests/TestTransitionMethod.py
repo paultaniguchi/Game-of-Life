00896004c5c2f255aabcc62d50935af9d5779262
Paul Taniguchi <paultani@yahoo.com>
2023-09-23 00:39:03 -0700

    Change test data for transition tests

--- GameofLife/src/tests/TestTransitionMethod.py
+++ GameofLife/src/tests/TestTransitionMethod.py
@@ -14,22 +14,28 @@ W |    | E
 import unittest
 from main.GameofLife import World
 
-class Test(unittest.TestCase):
-    # set up 3x3 grid for test
-    testworld = World(3,3)
-    exp_grid = [['e','z','a'],['a','z','z'],\
-                ['e','a','e']]
+class TestTransitionMethod(unittest.TestCase):
+    # use 5x5 grid
+    testworld = World(5,5)
+    exp_grid = [['d','z','d','d','z'],
+            ['d','e','d','e','d'],['d','a','a','a','d'],
+            ['d','d','a','d','d'],['z','d','d','d','z']]
 
     def setUp(self):
-        # set up 3x3 grid for test
-        self.testworld.set_grid([['d','a','a'],['a','a','a'],\
-                ['d','a','d']])
+        # set up 5x5 grid for test
+        self.testworld.set_grid([['d','a','d','d','a'],
+            ['d','d','d','d','d'],['d','a','a','a','d'],
+            ['d','d','a','d','d'],['a','d','d','d','a']])
 
-
+    # fill the 5x5 grid with dead cells as clean up
     def tearDown(self):
-        pass
+        self.testworld.set_grid([['d','d','d','d','d'],
+            ['d','d','d','d','d'],['d','d','d','d','d'],
+            ['d','d','d','d','d'],['d','d','d','d','d']])
 
-
+    # test for mark_for_transition method
+    # test that nodes marked as dead & alive in 1st half time step
+    # change to alive or dead cells in 2nd half time step
     def test_mark_for_transition(self):
         self.testworld.mark_for_transition()
         self.assertListEqual(self.testworld.grid,self.exp_grid)
